81403d7cdad726bb8c5229546797512f3a5d8dd9
Djailson1 <djailson_silva1@outlook.com>
2022-07-24 22:51:45 -0300

    enviado e-mail com bug

--- enviando-email/src/test/java/enviando/email/AppTest.java
+++ enviando-email/src/test/java/enviando/email/AppTest.java
@@ -4,13 +4,25 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.Properties;
 
+import javax.mail.Address;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest{
+	
+	private String userName = "djailsonemailformacaojavaweb@gmail.com";
+	private String senha = "tjusbmgdvjpmiwah";
+	
 	@Test
     public void testeEmail() { /*Olhe as configurações do smtp do seu email*/
 		
@@ -19,10 +31,29 @@ public class AppTest
 		Properties properties = new Properties();
 		properties.put("mail.smtp.auth", "true");/*autorização*/
 		properties.put("mail.smtp.starttls", "true");/*autenticação*/
-		properties.put("mail.host", "smtp.gmail.com");/*servidor gmail google*/
+		properties.put("mail.smtp.host", "smtp.gmail.com");/*servidor gmail google*/
 		properties.put("mail.smtp.port", "465");/*Porta do servidor google*/
 		properties.put("mail.smtp.socketFactory.port", "465");/*a porta a conectada pelo socket*/
 		properties.put("mail.smtp.socketFactory.class", "javax.net.sll.SLLSocketFactory");/*Classe socket de conexão ao SMTP*/
+		
+		Session session = Session.getInstance(properties, new Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+			return new PasswordAuthentication(userName, senha);
+			}
+		});
+		
+		Address[] toUser = InternetAddress.parse("djailson_silva1@outlook.com, djailson_silva1@hotmail.com");
+		
+		Message message = new MimeMessage(session);
+		message.setFrom(new InternetAddress(userName));/*Quem esta eniando*/
+		message.setRecipients(Message.RecipientType.TO, toUser);/*email de destino*/
+		message.setSubject("Chegou e-mail enviado com java");/*assunto do e-mail*/
+		message.setText("Olá programador, vocÊ acaba de receber um email enviado com java do curso Formação Java Web do Alex.");
+		
+		
+		
+		Transport.send(message);
 		
 		}catch (Exception e) {
 			e.printStackTrace();
