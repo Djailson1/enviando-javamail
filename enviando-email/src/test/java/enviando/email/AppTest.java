4963aad67e0509d8a157b2ac3cf7f349a26e9f9e
Djailson1 <djailson_silva1@outlook.com>
2022-07-29 22:04:36 -0300

    criando classe ObjetoEnviaEmail para reutilizar

--- enviando-email/src/test/java/enviando/email/AppTest.java
+++ enviando-email/src/test/java/enviando/email/AppTest.java
@@ -25,13 +25,19 @@ public class AppTest{
 	@Test
     public void testeEmail() throws Exception{ /*Olhe as configurações do smtp do seu email*/
 		
-		ObjetoEnviaEmail enviaEmail = new ObjetoEnviaEmail(null, null, null, null);
+		 ObjetoEnviaEmail enviaEmail = 
+				 new ObjetoEnviaEmail("djailson_silva1@hotmail.com, tianekessia25@hotmail.com",
+				                      "Djailson JDev",
+				                      "Testando e-mail com Java",
+				                      "Esse texto é a descrição do meu e-mail");
+		 
+		 enviaEmail.EnviarEmail();
 		
 		
 		/*caso o email não esteja sendo enviado então coloque
 		 * um tempo de espera porém isso só pode ser usado para testes*/
 		
-		
+		Thread.sleep(5000);
 		
     }
     
